e692c044fdad6f68df37b2282f009e3cc87fdcfe
Brandon Covington <brandoncovington@Interapts-MacBook-Pro.local>
2018-01-24 13:20:32 -0500

    Initial Commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by Brandon Covington on 1/24/18.
+//  Copyright © 2018 Brandon Covington. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
