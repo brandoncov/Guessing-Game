d524bb7babf3ca5784ca28c6ed3b4ac87c50c84a
Brandon Covington <brandoncovington@Interapts-MacBook-Pro.local>
2018-01-25 13:30:49 -0500

    initial commit

--- Guessing Game/main.swift
+++ Guessing Game/main.swift
@@ -6,7 +6,31 @@
 //  Copyright © 2018 Brandon Covington. All rights reserved.
 //
 
-import Foundation
+import Cocoa
 
-print("Hello, World!")
+//Generate a random number
+// TODO: Cast randomNumber to an int
+var randomNumber = arc4random_uniform(100)
+var guess: Int
+//Ask user input
+print("Please guess a number.")
+//Take user input
+guess = Int(readLine()!)!
+
+
+if randomNumber == guess {
+    print("YOU WON!!😁")
+} else if randomNumber > guess {
+    print("the number was higher!😔")
+} else if randomNumber < guess {
+    print("The Random Number was lower!😥")
+    
+}
+//Determine wrong or right(Loop until correct)
+//If correct ask if they want to play again
+//If incorrect tell them the correct answer and ask them to play again
+
+
+
+
 
